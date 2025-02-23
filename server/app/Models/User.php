59c5ecc61a37d7cf84070bf86d8ca499b936d88b
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2025-02-23 14:56:36 +0530

    Add models, factories, and seeders for categories, orders, products, and users

--- server/app/Models/User.php
+++ server/app/Models/User.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory; 
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
 
-class User extends Model
+class User extends Authenticatable
 {
-    //
+    use HasFactory, Notifiable; 
+
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'clerk_id',
+        'role',
+    ];
 }
