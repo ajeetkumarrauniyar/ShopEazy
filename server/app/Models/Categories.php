0957b91ee9f6b24743d012af8709d3bd6ae9d353
Ajeet Kumar <ajeetkumarrauniyar@gmail.com>
2025-02-23 15:33:01 +0530

    feat: Minor lintings

--- server/app/Models/Categories.php
+++ server/app/Models/Categories.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categories extends Model
 {
-    use HasFactory; 
+    use HasFactory;
 
-    protected $fillable = ['name']; 
+    protected $fillable = ['name'];
 }
